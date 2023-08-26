d6fdc37b4618596c70703d6a551cdd16222f34c0
Gabriel Oliveira <gabriel.dev04@gmail.com>
2023-08-26 15:11:09 -0300

    Connecting to mongodb with repository and service

--- src/main/java/com/oliveiragabrielc/workshopmongo/domain/User.java
+++ src/main/java/com/oliveiragabrielc/workshopmongo/domain/User.java
@@ -6,10 +6,12 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
