db3139a79534bf90ef0b88dfcc730414f8b6ffc3
Gabriel Oliveira <gabriel.dev04@gmail.com>
2023-08-27 12:41:13 -0300

    Implements method findById and create your excpetion

--- src/main/java/com/oliveiragabrielc/workshopmongo/services/UserService.java
+++ src/main/java/com/oliveiragabrielc/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.oliveiragabrielc.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.oliveiragabrielc.workshopmongo.domain.User;
 import com.oliveiragabrielc.workshopmongo.repositories.UserRepository;
+import com.oliveiragabrielc.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado!"));
 	}
 }
